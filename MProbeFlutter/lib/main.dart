368970ccf0e9eed2ec67ab2b40267e4142422de3
capitalch <capitalch@gmail.com>
2018-11-14 20:24:26 +0530

    init

--- MProbeFlutter/lib/main.dart
+++ MProbeFlutter/lib/main.dart
@@ -127,7 +127,8 @@ class MyApp extends StatelessWidget {
           );
         },
         'productDetails':(BuildContext context){
-          return(ProductDetails());
+          final prId = globals.Util.get('id');
+          return(ProductDetails(prId));
         },
         'mock':(BuildContext context){
           return(Mock());
--- MProbeFlutter/lib/productDetails.dart
+++ MProbeFlutter/lib/productDetails.dart
@@ -5,46 +5,50 @@ import 'ibuki.dart' as ibuki;
 //import 'globals.dart' as globals;
 
 class ProductDetails extends StatefulWidget {
+  final int prId;
+  ProductDetails(this.prId);
   @override
   ProductDetailsState createState() {
-    return ProductDetailsState();
+    return ProductDetailsState(prId);
   }
 }
 
 class ProductDetailsState extends State<ProductDetails> {
   dynamic subs;
+  final int prId;
+  dynamic productDetails = [];
+  ProductDetailsState(this.prId);
   @override
   void initState() {
-    subs = ibuki.filterOn('tunnel:get:product:details:on:prid').listen((d){
-      print(d);
+    subs = ibuki.filterOn('tunnel:get:product:details:on:prid').listen((d) {
+      setState(() {
+              productDetails=d['data'];
+              print(productDetails);
+            });
     });
-//    globals.httpPost('tunnel:get:product:details:on:prid',
-//        args: {'pr_id': globals.Util.get('id')}).then(
-//        (d){
-//          List<Map<String,dynamic>> resultSet = d;
-//        }
-//    );
-    ibuki.httpPost('tunnel:get:product:details:on:prid');
+    ibuki.httpPost('tunnel:get:product:details:on:prid', args: {'pr_id': prId});
     super.initState();
   }
 
   @override
-  void dispose(){
-    if(subs != null) subs.cancel();
+  void dispose() {
+    subs.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget wid = SafeArea(
-        child: Card(
-            margin: EdgeInsets.all(5.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Text('Card test')
-              ],
-            )));
+    Widget wid = Scaffold(appBar:AppBar(
+      title: Row(children: <Widget>[Text(productDetails.length > 0 ? '' : productDetails[0]['item'])],),
+    ),);
+    
+    // Widget wid = SafeArea(
+    //     child: Card(
+    //         margin: EdgeInsets.all(5.0),
+    //         child: Column(
+    //           mainAxisSize: MainAxisSize.max,
+    //           children: <Widget>[Text('Card test')],
+    //         )));
 
     return (wid);
   }
